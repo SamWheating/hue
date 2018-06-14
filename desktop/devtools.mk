f8e1bbb53f185db6ffa41b349b4301762e8bce1e
Matthew Bruce <matthew.bruce@shopify.com>
2018-06-14 15:58:26 -0400

    pip

--- desktop/devtools.mk
+++ desktop/devtools.mk
@@ -18,22 +18,11 @@
 # Developer tools - this file is only included in SDK build.
 
 # May require download from PyPI or whereever
-DEVTOOLS += \
-	ipdb[0.1dev-r1716] \
-	ipython[0.10] \
-	nose[0.11.3] \
-	coverage[3.7.1] \
-	nosetty[0.4] \
-	werkzeug[0.6] \
-	windmill[1.3] \
-	pylint[0.28.0]
 
 # Install/download dev tools for SDK into the virtual environment
 .PHONY: $(DEVTOOLS)
 $(DEVTOOLS):
-	@echo "--- Installing development tool: $@"
-	$(ENV_EASY_INSTALL) -f http://archive.cloudera.com/desktop-sdk-python-packages/ \
-	   -H pypi.python.org,archive.cloudera.com $(SETUPTOOLS_OPTS) $(subst ],,$(subst [,==,$@))
+	$(PIP) install -r requirements-devtools.txt
 
 $(BLD_DIR):
 	@mkdir -p $@
