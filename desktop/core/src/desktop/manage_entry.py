939bf8291c1824d4b83e0713624401f966954170
Sam Wheating <sam.wheating@shopify.com>
2018-10-24 15:54:46 -0400

    modified manage_entry.py for Django1.9.x compatibility

--- desktop/core/src/desktop/manage_entry.py
+++ desktop/core/src/desktop/manage_entry.py
@@ -35,19 +35,24 @@ def entry():
   _deprecation_check(sys.argv[0])
 
   from django.core.exceptions import ImproperlyConfigured
-  from django.core.management import execute_from_command_line, find_commands #, find_management_module
-  from django.core.management import LaxOptionParser
+  from django.core.management import execute_from_command_line, find_commands
+  from django.core.management import CommandParser
   from django.core.management.base import BaseCommand
 
   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desktop.settings')
 
-  # What's the subcommand being run?
-  # This code uses the same logic from django.core.management to handle command args
-  argv = sys.argv[:]
-  parser = LaxOptionParser(option_list=BaseCommand.option_list)
-  parser.parse_args(argv)
-  if len(argv) > 1:
-    prof_id = subcommand = argv[1]
+  if len(sys.argv) > 1:
+    subcommand = sys.argv[1]
+  parser = CommandParser(None, usage="%(prog)s subcommand [options] [args]", add_help=False)
+  parser.parse_known_args(sys.argv[2:])
+
+  if len(sys.argv) > 1:
+    prof_id = subcommand = sys.argv[1]
+    #Check if this is a CM managed cluster
+    if os.path.isfile(cm_config_file) and not cm_managed and not skip_reload:
+        print "ALERT: This appears to be a CM Managed environment"
+        print "ALERT: HUE_CONF_DIR must be set when running hue commands in CM Managed environment"
+        print "ALERT: Please run 'hue <command> --cm-managed'"
   else:
     prof_id = str(os.getpid())
 
