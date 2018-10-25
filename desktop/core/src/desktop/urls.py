87f99731a14c60c9cced9158b3574fe06535200c
Sam Wheating <sam.wheating@shopify.com>
2018-10-25 13:19:57 -0400

    Added include() to url constructor calls

--- desktop/core/src/desktop/urls.py
+++ desktop/core/src/desktop/urls.py
@@ -53,105 +53,105 @@ admin.autodiscover()
 
 # Some django-wide URLs
 dynamic_patterns = [
-  url(r'^accounts/login/$', 'desktop.auth.views.dt_login'),
-  url(r'^accounts/logout/$', 'desktop.auth.views.dt_logout', {'next_page': '/'}),
-  url(r'^profile$', 'desktop.auth.views.profile'),
-  url(r'^login/oauth/?$', 'desktop.auth.views.oauth_login'),
-  url(r'^login/oauth_authenticated/?$', 'desktop.auth.views.oauth_authenticated'),
+  url(r'^accounts/login/$', include('desktop.auth.views.dt_login')),
+  url(r'^accounts/logout/$', include('desktop.auth.views.dt_logout'), {'next_page': '/'}),
+  url(r'^profile$', include('desktop.auth.views.profile')),
+  url(r'^login/oauth/?$', include('desktop.auth.views.oauth_login')),
+  url(r'^login/oauth_authenticated/?$', include('desktop.auth.views.oauth_authenticated')),
 ]
 
 
 
 if USE_NEW_EDITOR.get():
   dynamic_patterns += [
-    url(r'^home$','desktop.views.home2'),
-    url(r'^home2$','desktop.views.home'),
-    url(r'^home_embeddable$','desktop.views.home_embeddable'),
+    url(r'^home$',include('desktop.views.home2')),
+    url(r'^home2$',include('desktop.views.home')),
+    url(r'^home_embeddable$',include('desktop.views.home_embeddable')),
   ]
 
 else:
   dynamic_patterns += [
-    url(r'^home$','desktop.views.home'),
-    url(r'^home2$','desktop.views.home2')
+    url(r'^home$',include('desktop.views.home')),
+    url(r'^home2$',include('desktop.views.home2'))
   ]
 
 dynamic_patterns += [
-  url(r'^logs$','desktop.views.log_view'),
-  url(r'^desktop/dump_config$','desktop.views.dump_config'),
-  url(r'^desktop/download_logs$','desktop.views.download_log_view'),
-  url(r'^desktop/get_debug_level','desktop.views.get_debug_level'),
-  url(r'^desktop/set_all_debug','desktop.views.set_all_debug'),
-  url(r'^desktop/reset_all_debug','desktop.views.reset_all_debug'),
-  url(r'^bootstrap.js$', 'desktop.views.bootstrap'), # unused
-  url(r'^desktop/prefs/(?P<key>\w+)?$', 'desktop.views.prefs'),
-  url(r'^desktop/status_bar/?$', 'desktop.views.status_bar'),
-  url(r'^desktop/debug/is_alive$','desktop.views.is_alive'),
-  url(r'^desktop/debug/is_idle$','desktop.views.is_idle'),
-  url(r'^desktop/debug/threads$', 'desktop.views.threads'),
-  url(r'^desktop/debug/memory$', 'desktop.views.memory'),
-  url(r'^desktop/debug/check_config$', 'desktop.views.check_config'),
-  url(r'^desktop/debug/check_config_ajax$', 'desktop.views.check_config_ajax'),
-  url(r'^desktop/log_frontend_event$', 'desktop.views.log_frontend_event'),
+  url(r'^logs$',include('desktop.views.log_view')),
+  url(r'^desktop/dump_config$',include('desktop.views.dump_config')),
+  url(r'^desktop/download_logs$',include('desktop.views.download_log_view')),
+  url(r'^desktop/get_debug_level',include('desktop.views.get_debug_level')),
+  url(r'^desktop/set_all_debug',include('desktop.views.set_all_debug')),
+  url(r'^desktop/reset_all_debug',include('desktop.views.reset_all_debug')),
+  url(r'^bootstrap.js$', include('desktop.views.bootstrap')), # unused
+  url(r'^desktop/prefs/(?P<key>\w+)?$', include('desktop.views.prefs')),
+  url(r'^desktop/status_bar/?$', include('desktop.views.status_bar')),
+  url(r'^desktop/debug/is_alive$',include('desktop.views.is_alive')),
+  url(r'^desktop/debug/is_idle$',include('desktop.views.is_idle')),
+  url(r'^desktop/debug/threads$', include('desktop.views.threads')),
+  url(r'^desktop/debug/memory$', include('desktop.views.memory')),
+  url(r'^desktop/debug/check_config$', include('desktop.views.check_config')),
+  url(r'^desktop/debug/check_config_ajax$', include('desktop.views.check_config_ajax')),
+  url(r'^desktop/log_frontend_event$', include('desktop.views.log_frontend_event')),
   
 
   # Mobile
-  url(r'^assist_m', 'desktop.views.assist_m'),
+  url(r'^assist_m', include('desktop.views.assist_m')),
   # Responsive
-  url(r'^responsive', 'desktop.views.responsive'),
+  url(r'^responsive', include('desktop.views.responsive')),
 
   # KO components, change to doc?name=ko_editor or similar
-  url(r'^ko_editor', 'desktop.views.ko_editor'),
-  url(r'^ko_metastore', 'desktop.views.ko_metastore'),
+  url(r'^ko_editor', include('desktop.views.ko_editor')),
+  url(r'^ko_metastore', include('desktop.views.ko_metastore')),
 
   # Jasmine
-  url(r'^jasmine', 'desktop.views.jasmine'),
+  url(r'^jasmine', include('desktop.views.jasmine')),
 
   # Unsupported browsers
-  url(r'^boohoo$','desktop.views.unsupported'),
+  url(r'^boohoo$',include('desktop.views.unsupported')),
 
   # Top level web page!
-  url(r'^$', 'desktop.views.index'),
+  url(r'^$', include('desktop.views.index')),
 ]
 
 dynamic_patterns += [
   # Tags
-  url(r'^desktop/api/tag/add_tag$', 'desktop.api.add_tag'),
-  url(r'^desktop/api/tag/remove_tag$', 'desktop.api.remove_tag'),
-  url(r'^desktop/api/doc/tag$', 'desktop.api.tag'),
-  url(r'^desktop/api/doc/update_tags$', 'desktop.api.update_tags'),
-  url(r'^desktop/api/doc/get$', 'desktop.api.get_document'),
+  url(r'^desktop/api/tag/add_tag$', include('desktop.api.add_tag')),
+  url(r'^desktop/api/tag/remove_tag$', include('desktop.api.remove_tag')),
+  url(r'^desktop/api/doc/tag$', include('desktop.api.tag')),
+  url(r'^desktop/api/doc/update_tags$', include('desktop.api.update_tags')),
+  url(r'^desktop/api/doc/get$', include('desktop.api.get_document')),
 
   # Permissions
-  url(r'^desktop/api/doc/update_permissions', 'update_permissions'),
+  url(r'^desktop/api/doc/update_permissions', include('update_permissions')),
 ]
 
 dynamic_patterns += [
-  url(r'^desktop/api2/doc/open?$', 'desktop.api2.open_document'),  # To keep before get_document
-  url(r'^desktop/api2/docs/?$', 'desktop.api2.search_documents'),  # search documents for current user
-  url(r'^desktop/api2/doc/?$', 'desktop.api2.get_document'),  # get doc/dir by path or UUID
+  url(r'^desktop/api2/doc/open?$', include('desktop.api2.open_document')),  # To keep before get_document
+  url(r'^desktop/api2/docs/?$', include('desktop.api2.search_documents')),  # search documents for current user
+  url(r'^desktop/api2/doc/?$', include('desktop.api2.get_document')),  # get doc/dir by path or UUID
 
-  url(r'^desktop/api2/doc/move/?$', 'desktop.api2.move_document'),
-  url(r'^desktop/api2/doc/mkdir/?$', 'desktop.api2.create_directory'),
-  url(r'^desktop/api2/doc/update/?$', 'desktop.api2.update_document'),
-  url(r'^desktop/api2/doc/delete/?$', 'desktop.api2.delete_document'),
-  url(r'^desktop/api2/doc/restore/?$', 'desktop.api2.restore_document'),
-  url(r'^desktop/api2/doc/share/?$', 'desktop.api2.share_document'),
+  url(r'^desktop/api2/doc/move/?$', include('desktop.api2.move_document')),
+  url(r'^desktop/api2/doc/mkdir/?$', include('desktop.api2.create_directory')),
+  url(r'^desktop/api2/doc/update/?$', include('desktop.api2.update_document')),
+  url(r'^desktop/api2/doc/delete/?$', include('desktop.api2.delete_document')),
+  url(r'^desktop/api2/doc/restore/?$', include('desktop.api2.restore_document')),
+  url(r'^desktop/api2/doc/share/?$', include('desktop.api2.share_document')),
 
-  url(r'^desktop/api2/doc/export/?$', 'desktop.api2.export_documents'),
-  url(r'^desktop/api2/doc/import/?$', 'desktop.api2.import_documents'),
+  url(r'^desktop/api2/doc/export/?$', include('desktop.api2.export_documents')),
+  url(r'^desktop/api2/doc/import/?$', include('desktop.api2.import_documents')),
 
-  url(r'^desktop/api/search/entities/?$', 'desktop.api2.search_entities'),
-  url(r'^desktop/api/search/entities_interactive/?$', 'desktop.api2.search_entities_interactive'),
+  url(r'^desktop/api/search/entities/?$', include('desktop.api2.search_entities')),
+  url(r'^desktop/api/search/entities_interactive/?$', include('desktop.api2.search_entities_interactive')),
 ]
 
 # Default Configurations
 dynamic_patterns += [
-  url(r'^desktop/api/configurations/?$', 'desktop.configuration.api.default_configurations'),
-  url(r'^desktop/api/configurations/user/?$', 'desktop.configuration.api.app_configuration_for_user'),
-  url(r'^desktop/api/configurations/delete/?$', 'desktop.configuration.api.delete_default_configuration'),
+  url(r'^desktop/api/configurations/?$', include('desktop.configuration.api.default_configurations')),
+  url(r'^desktop/api/configurations/user/?$', include('desktop.configuration.api.app_configuration_for_user')),
+  url(r'^desktop/api/configurations/delete/?$', include('desktop.configuration.api.delete_default_configuration')),
 ]
 
-dynamic_patterns += [url(r'^desktop/api/users/autocomplete', 'useradmin.views.list_for_autocomplete')]
+dynamic_patterns += [url(r'^desktop/api/users/autocomplete', include('useradmin.views.list_for_autocomplete'))]
 
 # Metrics specific
 if METRICS.ENABLE_WEB_METRICS.get():
